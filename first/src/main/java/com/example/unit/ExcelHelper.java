34a53a00acf1a80dcc72b36409cba3618248d94b
HBING <598003677@qq.com>
2017-12-18 21:56:46 +0800

    fix

--- first/src/main/java/com/example/unit/ExcelHelper.java
+++ first/src/main/java/com/example/unit/ExcelHelper.java
@@ -1,9 +1,15 @@
 package com.example.unit;
 
+import java.io.File;
+
 /**
  * Created by 59800 on 2017/12/18.
  */
 
 public class ExcelHelper {
     public static String FILENAME = "SLR.xls";
+
+    public static void getExcel() {
+        File file = new File(FILENAME);
+    }
 }
